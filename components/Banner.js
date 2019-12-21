69bb194f02f69c7e0aeeb2aed6d1b34d1e30ace7
Pathinya <gift_01210@hotmail.com>
2019-12-22 01:14:02 +0700

    [ADD] video button at the bottom-right

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
-
+const LineHeight = {
+    lineHeight: '20px'
+}
 export default class Banner extends Component {
     render() {
         return (
@@ -9,7 +11,13 @@ export default class Banner extends Component {
                     <h3 style={{paddingTop: '12%', paddingBottom: '15%'}}>I am EXERON</h3>
                     <p>The beginning of the</p>
                     <img src="../static/images/off-grid.png"/>
-                    <h6 style={{fontWeight: 'bold'}}>REVOLUTION</h6>
+                    <h5 style={{fontWeight: 'bold'}} className="mt-2">REVOLUTION</h5>
+                </div>
+                <br/>
+                <div className="pr-5 text-right" style={LineHeight}>
+                    <a className="text-white font-weight-light video-link" style={{fontSize: "30px"}}>Videos <span className="text-primary video-link">➜</span></a>
+                    <br/>
+                    <a className="text-primary" style={{paddingRight: '2.5%'}}><span className="video-link"><b>Meet the EXERON</b> technology</span></a>
                 </div>
             </div>
         )
